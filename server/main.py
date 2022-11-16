c80a030f129e3c1dadde5c83584df333f81b9b5d
Md.Abdullah <abdullah15-3842@diu.edu.bd>
2022-11-16 08:28:27 -0600

    fix

--- server/main.py
+++ server/main.py
@@ -1,9 +1,8 @@
 import glob
-from logging import exception
-from pydoc import describe
 import shutil
-import os
 import runpy
+import os
+import time
 
 text_source_path = '../source/*.txt'
 py_source_path = '../source/*.py'
@@ -12,31 +11,44 @@ temp = '../source/temp/'
 postfix = [1, 2, 3]
 
 while True:
-
+    
+    os.mkdir('../source/temp')
     text_source_object = glob.glob(text_source_path)
     py_source_object = glob.glob(py_source_path)
     if len(text_source_object) > 0:
+            
         # reading source file
-        with open('../source/file.txt', 'r') as file:
-            lines = file.readlines()
-            file.close()
-
-        #write and spliting
         for file_path in text_source_object:
-            object_name = file_path.split('//')[-1].split('.')
+            with open(file_path,'r') as file:
+                lines = file.readlines()
+                file.close()
+
+             #write and spliting
+            
+            object_name = file_path.split('\\')[-1].split('.')
             prefix = object_name[0]
             postfix = object_name[1]
-
+            print(prefix)
             for item in range(1, 4):
-                with open(f'../destination/temp/{prefix}_{item}.txt', 'w') as file:
-                    file.writelines(lines[:10*item])
-                    file.close()
+                    with open(f'../source/temp/{prefix}_{item}.txt', 'w') as file:
+                        file.writelines(lines[:10*item])
+                        file.close()
+
+            
         shutil.make_archive(base_name=f'../source/file',
-                            format='zip', root_dir='../destination/temp/')
-        shutil.move(src='../source/file.zip', dst='../destination/file.zip')
+                            format='zip', root_dir='../source/temp/')
+        
+        shutil.copy(src='../source/file.zip', dst='../destination/file.zip')
+        
         shutil.unpack_archive(filename='../destination/file.zip',
                               extract_dir='../destination/')
-
+        
+       
+        
+        os.remove("../destination/file.zip")
+        shutil.rmtree("../source/temp/")
+        time.sleep(0.1)
+    
     if len(py_source_object) > 0:
         for p_file in py_source_object:
             try:
